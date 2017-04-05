dda9efca5840edfcbe304a71264f2db3ec2c099e
Raymond Perminov <raymondproduction@ukr.net>
2017-04-06 01:49:43 +0300

    Added userId in model Beer for store the id of the user that owns it

--- models/beer.js
+++ models/beer.js
@@ -2,13 +2,11 @@
 var mongoose = require('mongoose');
 
 // Define our beer schema
-// Created a Mongoose schema which maps to a MongoDB
-// collection and defines the shape of the documents
-// within that collection
 var BeerSchema   = new mongoose.Schema({
   name: String,
   type: String,
-  quantity: Number
+  quantity: Number,
+  userId: String
 });
 
 // Export the Mongoose model
