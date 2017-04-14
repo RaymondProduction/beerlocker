58ee75f86fb019991c05359c6da29658a0e9f767
Raymond Perminov <raymondproduction@ukr.net>
2017-04-14 14:51:40 +0300

    Find of the errors when post request for token

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -15,8 +15,12 @@ server.serializeClient(function(client, callback) {
 
 // Register deserialization function
 server.deserializeClient(function(id, callback) {
-  Client.findOne({ _id: id }, function (err, client) {
-    if (err) { return callback(err); }
+  Client.findOne({
+    _id: id
+  }, function(err, client) {
+    if (err) {
+      return callback(err);
+    }
     return callback(null, client);
   });
 });
@@ -33,7 +37,9 @@ server.grant(oauth2orize.grant.code(function(client, redirectUri, user, ares, ca
 
   // Save the auth code and check for errors
   code.save(function(err) {
-    if (err) { return callback(err); }
+    if (err) {
+      return callback(err);
+    }
 
     callback(null, code.value);
   });
@@ -41,15 +47,33 @@ server.grant(oauth2orize.grant.code(function(client, redirectUri, user, ares, ca
 
 // Exchange authorization codes for access tokens
 server.exchange(oauth2orize.exchange.code(function(client, code, redirectUri, callback) {
-  Code.findOne({ value: code }, function (err, authCode) {
-    if (err) { return callback(err); }
-    if (authCode === undefined) { return callback(null, false); }
-    if (client._id.toString() !== authCode.clientId) { return callback(null, false); }
-    if (redirectUri !== authCode.redirectUri) { return callback(null, false); }
+  Code.findOne({
+    value: code
+  }, function(err, authCode) {
+    if (err) {
+      console.log('1');
+      return callback(err);
+    }
+    if (authCode === undefined) {
+      console.log('2');
+      return callback(null, false);
+    }
+    if (client._id.toString() !== authCode.clientId) {
+      console.log('3');
+      return callback(null, false);
+    }
+
+    if (redirectUri !== authCode.redirectUri) {
+      console.log('4');
+      console.log('"=>"'+redirectUri+'" "'+authCode.redirectUri+'"');
+      return callback(null, false);
+    }
 
     // Delete auth code now that it has been used
-    authCode.remove(function (err) {
-      if(err) { return callback(err); }
+    authCode.remove(function(err) {
+      if (err) {
+        return callback(err);
+      }
 
       // Create a new access token
       var token = new Token({
@@ -59,8 +83,10 @@ server.exchange(oauth2orize.exchange.code(function(client, code, redirectUri, ca
       });
 
       // Save the access token and check for errors
-      token.save(function (err) {
-        if (err) { return callback(err); }
+      token.save(function(err) {
+        if (err) {
+          return callback(err);
+        }
 
         callback(null, token);
       });
@@ -70,19 +96,27 @@ server.exchange(oauth2orize.exchange.code(function(client, code, redirectUri, ca
 
 // User authorization endpoint
 exports.authorization = [
-  server.authorization(function(clientId, redirectUri, callback) {
+    server.authorization(function(clientId, redirectUri, callback) {
 
-    Client.findOne({ id: clientId }, function (err, client) {
-      if (err) { return callback(err); }
+      Client.findOne({
+        id: clientId
+      }, function(err, client) {
+        if (err) {
+          return callback(err);
+        }
 
-      return callback(null, client, redirectUri);
-    });
-  }),
-  function(req, res){
-    res.render('dialog', { transactionID: req.oauth2.transactionID, user: req.user, client: req.oauth2.client });
-  }
-]
-// User decision endpoint
+        return callback(null, client, redirectUri);
+      });
+    }),
+    function(req, res) {
+      res.render('dialog', {
+        transactionID: req.oauth2.transactionID,
+        user: req.user,
+        client: req.oauth2.client
+      });
+    }
+  ]
+  // User decision endpoint
 exports.decision = [
   server.decision()
 ]
@@ -93,10 +127,10 @@ exports.token = [
   server.errorHandler()
 ]
 
-function uid (len) {
-  var buf = []
-    , chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    , charlen = chars.length;
+function uid(len) {
+  var buf = [],
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+    charlen = chars.length;
 
   for (var i = 0; i < len; ++i) {
     buf.push(chars[getRandomInt(0, charlen - 1)]);
